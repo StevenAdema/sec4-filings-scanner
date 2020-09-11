3986ab8cfca0597f961eccd9720120a0f19cf618
stevenadema <stevenadema@gmail.com>
2020-09-10 21:40:11 -0400

    filtering by stock bought

--- src/main.py
+++ src/main.py
@@ -5,9 +5,9 @@ pd.options.mode.chained_assignment = None  # supress copy warning
 import numpy as np
 import urllib.request
 np.set_printoptions(linewidth=300)
-pd.set_option('display.max_columns', None)  
+pd.set_option('display.max_columns', None)
 pd.set_option('display.expand_frame_repr', False)
-pd.set_option('max_colwidth', 50)
+pd.set_option('max_colwidth', 30)
 
 
 def main():
@@ -20,72 +20,76 @@ def main():
         config = json.load(f)
 
     # Url for API call
-    # TOKEN = config["credentials"]["key"]
-    # API = 'https://api.sec-api.io?token=' + TOKEN
+    TOKEN = config["credentials"]["key"]
+    API = 'https://api.sec-api.io?token=' + TOKEN
 
-    # # Create filter parameters to send to the API
-    # filter = "formType:\"4\" AND ticker:(NOT \"\") AND formType:(NOT \"N-4\") AND formType:(NOT \"4/A\") AND filedAt:[2020-06-20 TO 2020-06-21]"
-    # payload = {
-    #     "query": {"query_string": {"query": filter}},
-    #     "from": "0",
-    #     "size": "200",
-    #     "sort": [{"filedAt": {"order": "desc"}}]
-    # }
+    # Create filter parameters to send to the API
+    filter = "formType:\"4\" AND ticker:(NOT \"\") AND formType:(NOT \"N-4\") AND formType:(NOT \"4/A\") AND filedAt:[2019-09-10 TO 2019-09-10]"
+    payload = {
+        "query": {"query_string": {"query": filter}},
+        "from": "0",
+        "size": "200",
+        "sort": [{"filedAt": {"order": "desc"}}]
+    }
 
-    # # Format payload to JSON bytes
-    # jsondata = json.dumps(payload)
-    # jsondataasbytes = jsondata.encode('utf-8')
+    # Format payload to JSON bytes
+    jsondata = json.dumps(payload)
+    jsondataasbytes = jsondata.encode('utf-8')
 
-    # # Send request
-    # print('sending request')
-    # req = urllib.request.Request(API)
+    # Send request
+    print('sending request')
+    req = urllib.request.Request(API)
 
-    # # set the correct HTTP header: Content-Type = application/json
-    # req.add_header('Content-Type', 'application/json; charset=utf-8')
-    # # set the correct length of your request
-    # req.add_header('Content-Length', len(jsondataasbytes))
+    # set the correct HTTP header: Content-Type = application/json
+    req.add_header('Content-Type', 'application/json; charset=utf-8')
+    # set the correct length of your request
+    req.add_header('Content-Length', len(jsondataasbytes))
 
-    # # send the request to the API
-    # response = urllib.request.urlopen(req, jsondataasbytes)
+    # send the request to the API
+    response = urllib.request.urlopen(req, jsondataasbytes)
 
-    # # read the response
-    # response_body = response.read()
-    # # transform the response into JSON
-    # filings = json.loads(response_body.decode("utf-8"))
+    # read the response
+    response_body = response.read()
+    # transform the response into JSON
+    filings = json.loads(response_body.decode("utf-8"))
 
-    # # write json response to file.
-    # with open(r'..\data\data.json', 'w') as f:
-    #     json.dump(filings, f)
+    # write json response to file.
+    with open(r'..\data\data.json', 'w') as f:
+        json.dump(filings, f)
 
-    # # Use saved JSON
-    # with open(r'..\data\data.json', 'r') as f:
-    #     filings = json.load(f)
+    # Use saved JSON
+    with open(r'..\data\data.json', 'r') as f:
+        filings = json.load(f)
 
-    # print('generate DataFrame')
-    # # Load results to DataFrame
-    # df = pd.DataFrame(filings['filings'])
+    print('generate DataFrame')
+    # Load results to DataFrame
+    df = pd.DataFrame(filings['filings'])
+    print(df.shape)
 
-    # # Create DataFrame
-    # df = df[config["dataframe"]["columns_shortlist"]]
+    # Create DataFrame
+    df = df[config["dataframe"]["columns_shortlist"]]
 
-    # # Add columns additional columns
-    # for new_column in config["dataframe"]["columns_new"]:
-    #     df[new_column] = np.nan
+    # Add columns additional columns
+    for new_column in config["dataframe"]["columns_new"]:
+        df[new_column] = np.nan
 
-    # df = df[config["dataframe"]["column_order"]]
-    # df = df[df['companyName'] != df['rptOwnerName']]
+    df = df[config["dataframe"]["column_order"]]
+    df = df[df['companyName'] != df['rptOwnerName']]
 
-    # df2 = df[0:0]
-    # print('read to new DataFrame')
-    # reader.read_sec4_to_dataframe(df, df2)
+    print('read to new DataFrame')
+    df = reader.filter_has_ticker(df)
+    df2 = df[0:0]
+    print(df.shape)
+    reader.read_sec4_to_dataframe(df, df2)
+    print('read')
+    print(df2.shape)
 
     # df2.to_pickle(r'..\data\data.pkl')
-    df2 = pd.read_pickle(r'..\data\data.pkl')
+    # df2 = pd.read_pickle(r'..\data\data.pkl')
 
+    df2 = reader.get_only_bought(df2)
 
-    reader.get_only_bought(df2)
-
-    df2 = df2[df2['companyName'] != df2['rptOwnerName']]
+    print(df2)
 
     # Write df to CSV
     # df2 = df2[df2['expirationDate'] != '']
--- src/sec4_reader.py
+++ src/sec4_reader.py
@@ -4,6 +4,7 @@ pd.options.mode.chained_assignment = None  # supress copy warning
 import numpy as np
 import urllib.request
 import xml.etree.cElementTree as ET
+import time
 import re
 import sys
 
@@ -11,44 +12,33 @@ with open(r'..\config\config.json') as f:
     config = json.load(f)
 
 
-def read_sec4_to_dataframe(df, df2, ):
+def read_sec4_to_dataframe(df, df2):
     for i, row in df.iterrows():
         xml = df['linkToTxt'].iloc[i]
         xml_root = download_xml(xml)
 
-        derivativeTransactions = xml_root.findall(
-            "./derivativeTable/derivativeTransaction")
-        nonDerivativeTransactions = xml_root.findall(
-            "./nonDerivativeTable/nonDerivativeTransaction")
+        derivativeTransactions = xml_root.findall("./derivativeTable/derivativeTransaction")
+        nonDerivativeTransactions = xml_root.findall("./nonDerivativeTable/nonDerivativeTransaction")
         allTransactions = derivativeTransactions + nonDerivativeTransactions
 
         for t in allTransactions:
             securityTitle = read_tag(t, './securityTitle/value')
             transactionDate = read_tag(t, './transactionDate/value')
-            exerciseShares = float(
-                read_tag(t, './transactionAmounts/transactionShares/value'))
+            exerciseShares = float(read_tag(t, './transactionAmounts/transactionShares/value'))
             expirationDate = read_tag(t, './expirationDate/value')
-            exercisePrice = float(
-                read_tag(t, './transactionAmounts/transactionPricePerShare/value'))
+            exercisePrice = float(read_tag(t, './transactionAmounts/transactionPricePerShare/value'))
+            boughtSold = read_tag(t, './transactionAmounts/transactionAcquiredDisposedCode/value')
             transactionValue = exercisePrice * exerciseShares
-            boughtSold = read_tag(
-                t, './transactionAmounts/transactionAcquiredDisposedCode/value')
-
             ticker_element = xml_root.findall("./issuer")[0]
-            tradingSymbol = read_tag(
-                ticker_element, "./issuerTradingSymbol", '')
+            tradingSymbol = read_tag(ticker_element, "./issuerTradingSymbol", '')
 
             # Owner Relationship
             owner = xml_root.findall("./reportingOwner")[0]
             rptOwnerName = read_tag(owner, './reportingOwnerId/rptOwnerName')
-            isDirector = int(
-                read_tag(owner, './reportingOwnerRelationship/isDirector', 0))
-            isOfficer = int(
-                read_tag(owner, './reportingOwnerRelationship/isOfficer', 0))
-            isTenOwner = int(
-                read_tag(owner, './reportingOwnerRelationship/isTenPercentOwner', 0))
-            isOther = int(
-                read_tag(owner, './reportingOwnerRelationship/isOther', 0))
+            isDirector = int(read_tag(owner, './reportingOwnerRelationship/isDirector', 0))
+            isOfficer = int(read_tag(owner, './reportingOwnerRelationship/isOfficer', 0))
+            isTenOwner = int(read_tag(owner, './reportingOwnerRelationship/isTenPercentOwner', 0))
+            isOther = int(read_tag(owner, './reportingOwnerRelationship/isOther', 0))
             if isDirector == 1:
                 owner = 'Director'
             elif isOfficer == 1:
@@ -88,9 +78,34 @@ def read_tag(root, path, exc=np.nan):
     return v
 
 
+def filter_has_ticker(df):
+    """Remove rows from the DataFrame that don't have ticker
+
+    Args:
+        df: a DataFrame object
+    """
+    df = df[df['ticker'].str.len() > 1]
+    df.reset_index(drop=True, inplace=True)
+    return df
+
+
 def get_only_bought(df2):
-    df = df2.groupby(['ticker', 'transactionDate']).agg('boughtSold')
-    print(df)
+    """Takes a dataframe and filters only records where the only trades were buys
+
+    Args:
+        df: a DataFrame object
+
+    Returns
+        Returns the same dataframe less the filings continaing both buys and sells or only sells
+    """
+    df = df2.groupby(['ticker', 'transactionDate'], as_index=False)['boughtSold'].apply(''.join).reset_index(name='buysAndSells')
+    df['onlyBought'] = np.where(df['buysAndSells'].str.contains('D'), False, True)
+    df = df[df['onlyBought']]
+
+    df2 = pd.merge(df2, df, how='right', left_on=['ticker', 'transactionDate'], right_on=['ticker', 'transactionDate'])
+    print(df2.shape)
+    return df2
+
 
 def download_xml(url, tries=1):
     try:
